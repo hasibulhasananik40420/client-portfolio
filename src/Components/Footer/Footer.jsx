b378e628367e34d680674ec573ae98d29947f9d1
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2023-11-25 15:59:37 +0600

    updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import chad from "../../assets/chad.png";
 
 const Footer = () => {
   const { pathname } = useLocation();
+
   return (
     <div className="bg-[#F3F7F8] md:h-[200px] h-full w-full mt-24">
       <div className="max-w-containerX mx-auto md:flex md:items-center md:gap-32 px-4 md:px-8 lg:px-4 py-16 md:py-12">
@@ -48,8 +51,19 @@ const Footer = () => {
                 </div>
               </Link>
             </>
-          ) :
-           (
+          ) : pathname === "/driver-app" ? (
+            <>
+               <Link to={'/transportation-optimizer'} className="flex cursor-pointer mt-3">
+                <div className="w-16 h-14 bg-[#06D6A0]"></div>
+                <div className="h-14 bg-white shadow w-[280px] px-6 py-[6px]">
+                  <h1 className="text-[16px] text-[#2B2B2B] font-Montserrat font-medium mt-2">
+                    {" "}
+                    Transportation Optimizer
+                  </h1>
+                </div>
+              </Link>
+            </>
+          ) : (
             ''
           )}
         </div>
