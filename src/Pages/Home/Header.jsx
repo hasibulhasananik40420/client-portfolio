77e036c1df4ea959b732ed99ba4e6a6994c1a932
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2024-02-07 02:06:02 +0600

    updated

--- src/Pages/Home/Header.jsx
+++ src/Pages/Home/Header.jsx
@@ -47,7 +47,7 @@ const Header = () => {
       <div className="lg:flex justify-center lg:gap-6 2xl:gap-6">
         <div className="">
           <div className="w-full">
-            <div className="lg:w-[335px] xl:w-[335px] 2xl:w-[335px] rounded-lg md:mx-auto lg:mx-0 2xl:h-[330px] lg:h-[330px] xl:h-[330px] md:h-[600px] h-[350px]">
+            <div className="lg:w-[339px] xl:w-[339px] 2xl:w-[339px] rounded-lg md:mx-auto lg:mx-0 2xl:h-[330px] lg:h-[330px] xl:h-[330px] md:h-[600px] h-[350px]">
               <img
                 className="w-full h-full  object-cover rounded-lg"
                 src={chad2}
